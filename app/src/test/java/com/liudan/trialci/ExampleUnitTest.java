9c005e7e3caec9876aaf6268af79484c44940040
liudan <amyliu@xingren.com>
2016-09-04 09:31:20 +0800

    test

--- app/src/test/java/com/liudan/trialci/ExampleUnitTest.java
+++ app/src/test/java/com/liudan/trialci/ExampleUnitTest.java
@@ -1,42 +1,13 @@
 package com.liudan.trialci;
 
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
-
-@RunWith(AndroidJUnit4.class)
-@LargeTest
-public class ExampleUnitTest extends BaseActivityTest {
-
-    public ExampleUnitTest(){
-
-    }
-
-
-    @Before
-    public void createLog() {
-
-    }
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class) {
-        @Override
-        protected void beforeActivityLaunched() {
-            setUp();
-        }
-    };
-
+public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
